5482af8198016922154ecbfcbfc18da4144e3b00
unknown <manuel.presentaciongr@gmail.com>
2025-01-13 13:05:49 +0100

    cambios

--- libros.php
+++ libros.php
@@ -13,6 +13,24 @@ class libros{
         $this->titulo = $titulo;
         $this->estado = $estado;
     }
+
+    public function mostrarLibro(){
+        $sentencia = "SELECT * FROM libros"; // Creamos la sentencia
+        $consulta = $this->conn->prepare($sentencia); // Creamos la consulta
+        $consulta->bind_param("ss", $nom, $psw); // Creamos los parametros
+        $consulta->bind_result($count); // Creamos el resultado
+
+        $consulta->execute(); // Ejecutamos la consulta
+        $consulta->fetch(); // Obtenemos el resultado
+
+        $existe = false; // Variable para comprobar si el usuario existe
+
+        if($count == 1) $existe = true; // Si el usuario existe
+
+        $consulta->close(); // Cerramos la consulta
+        return $existe; // Devolvemos el valor de la variable
+    }
+
 }
 
 ?>
