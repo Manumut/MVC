97b9293bf0c30d42eabff2d2352a98a65da6b346
unknown <manuel.presentaciongr@gmail.com>
2025-01-13 11:35:35 +0100

    cambios

---
+++ cred.php
@@ -0,0 +1,13 @@
+<?php
+
+define('USUARIO_CON', 'con');
+define('PASS_CON', 'ola');
+
+
+?>
+
+
+<!-- 
+en la base de datos entro en cuentas de usuarios y creo mi usuario
+    en class_db.php pondremos un required_once('../../cred.php);  y en el nombre de usuario pondremos USUARIO_CON y en la contraseña PASS_CON
+-->
