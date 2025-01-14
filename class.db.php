c5238c7d1aba92761727872a3a25c0bac1aed6b9
unknown <manuel.presentaciongrgmail.com>
2025-01-14 11:39:14 +0100

    cambios

--- class.db.php
+++ class.db.php
@@ -1,9 +1,12 @@
 <?php
     class db{ // Clase db
         private $conn; // Variable de conexion
-        
         public function __construct(){ // Constructor
             $this->conn = new mysqli("Localhost", USUARIO_CON, PSW_CON, "biblioteca"); // Creamos la conexion
+        }
+         // Método para obtener la conexión mysqli
+         public function getConn() {
+            return $this->conn;
         }
     }
 ?>
