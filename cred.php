edf99dfbdb73493e51f3a522efb4d3e8289e1406
unknown <manuel.presentaciongr@gmail.com>
2025-01-14 12:03:08 +0100

    cambios

--- cred.php
+++ cred.php
@@ -6,8 +6,4 @@ define('PSW_CON', 'ola');
 
 ?>
 
-
-<!-- 
-en la base de datos entro en cuentas de usuarios y creo mi usuario
-    en class_db.php pondremos un required_once('../../cred.php);  y en el nombre de usuario pondremos USUARIO_CON y en la contraseña PASS_CON
--->
+<!-- meter esto en htdocs -->
