49cfcedb2aee34eeb632f5c405eeb7833013aa03
unknown <manuel.presentaciongr@gmail.com>
2025-01-20 11:30:47 +0100

    ejer3

---
+++ ej3/class.db.php
@@ -0,0 +1,13 @@
+<?php
+require_once('../../../cred.php');
+    class db{ // Clase db
+        private $con; // Variable de conexion
+        public function __construct(){ // Constructor
+            $this->con = new mysqli("Localhost", USUARIO_CON, PSW_CON, "biblioteca"); // Creamos la conexion
+        }
+         // Método para obtener la conexión mysqli
+         public function getCon() {
+            return $this->con;
+        }
+    };
+?>
